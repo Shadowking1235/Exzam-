29be127feb2362a13604fe5155a3170e19a173d4
Shadowking1235 <43701529+Shadowking1235@users.noreply.github.com>
2019-03-23 12:27:07 +0400

    Add files via upload

---
+++ Exzam.proposal.py
@@ -0,0 +1,13 @@
+"""""""""
+My first project will consist of a Hospital management project to understand OOP (object oriented programming)
+1. https://code-projects.org/simple-hospital-management-system-in-python-with-source-code/
+My second project will consist of a simple Inventory_project to understand queue and parallel programming.
+2.https://code-projects.org/simple-inventory-system-in-python-with-source-code/
+3.My third project consist of a Password_Generator to understand more of practical programming.
+ https://code-projects.org/password-generator-in-python-with-source-code/
+*4. My third project which is also my favourite is a chess 
+game in python to understanding more about the chess algorithm
+https://gist.github.com/rsheldiii/2993225
+5. My last project consist if a Ludo board game I also choose this project to understand more about about board games 
+https://code-projects.org/simple-2d-ludo-game-in-python-with-source-code/
+"""""""""
